e95d346c8ed286d5c0ba91b8ea20703ddf9c0de6
nicole.peoples <nicole.peoples@zonarsystems.com>
2017-02-06 21:22:00 -0800

    setup firebase

--- src/app/components/auth/auth.module.js
+++ src/app/components/auth/auth.module.js
@@ -1,5 +1,22 @@
 angular
-  .module('components.auth', [
-    'ui.router',
-    'firebase'
-  ]);
+    .module('components.auth', [
+        'ui.router',
+        'firebase'
+    ])
+    .config(function ($firebaseRefProvider) {
+        
+        var config = {
+            apiKey: "AIzaSyBpc93tiapkeb7OiAu0hnWWjd9adJGcT0c",
+            authDomain: "contacts-manager-6ee68.firebaseapp.com",
+            databaseURL: "https://contacts-manager-6ee68.firebaseio.com",
+            storageBucket: "contacts-manager-6ee68.appspot.com",
+            messagingSenderId: "230671818167"
+        };
+
+        $firebaseRefProvider
+            .registerUrl({
+                default: config.databaseURL,
+                contacts: config.databaseURL + '/contacts',
+            });
+        firebase.initializeApp(config);
+    });
